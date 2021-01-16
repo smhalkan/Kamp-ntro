39b252053bec737a16eb83395fce1437e32578fa
Semih Alkan <smhalkan89@gmail.com>
2021-01-16 23:58:30 +0300

    16 Ocak Ders Sonu

---
+++ Metotlar/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            //type-safe -- tip güvenli
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------------");
+            }
+
+            Console.WriteLine("--------Metotlar---------");
+
+            //instance- örnek
+            //encapsulation -- kapsülleme , ayrı ayrı yazmaktansa düzensiz olmasını istemediğin şeyi düzene sokmak demek
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12, 8);
+
+
+        }
+    }
+}
+// metotlar tekrar tekrar kullanımı kolaylaştırıyor
+// dont repeat yourself - dry - clean Code - Best Practice
