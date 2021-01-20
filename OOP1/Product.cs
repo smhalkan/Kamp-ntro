4f987ad0a7440a8d60cd143e556ccffcfcb73b8f
Semih Alkan <smhalkan89@gmail.com>
2021-01-20 23:51:11 +0300

    20 OCAK DERS SONU

---
+++ OOP1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{   //snippet - hazır kodlar demek . tab tab tuşuna iki kere basıldığında hızlıca tanımlar
+    class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int CategoryId { get; set; }
+        public int UnitsInStock { get; set; }
+
+
+        //CRUD operasyonları e ticaret sitelerinde ürün ekleme silme güncelleme vb operasyonlarıdır.
+    }
+}
